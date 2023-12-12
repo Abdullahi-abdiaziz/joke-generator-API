0e8b8b318c945506526a39c2f19a6fa354811f84
Abdullahi Abdiaziz yusuf <abdullahikawte2019@gmail.com>
2023-12-12 08:35:34 +0300

    Upload files

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [joke, setJoke] = useState("");
+
+  const API_KEY = "Q4HafUiEkvLOL6LblG9c4g==R4JTF2ZawN70SAap";
+  const URL = `https://api.api-ninjas.com/v1/jokes`;
+
+  const fetchJoke = async () => {
+    try {
+      const response = await fetch(URL, {
+        method: "GET",
+        headers: {
+          "X-Api-Key": API_KEY,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+      setJoke(result[0].joke);
+    } catch (error) {
+      console.error("Error: ", error.message);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Random Joke Generator</h1>
+      <div className="joke">
+        {joke && <p>{joke}</p>}
+
+        <button onClick={fetchJoke}>New Joke!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
